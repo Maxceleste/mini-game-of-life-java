05e1e17b681cabdabef075e356abf89f305adebe
Max Paulo Simão Teixeira <maxpauloteixeira@gmail.com>
2022-12-09 16:50:44 -0300

    Maked a random pattern generator

--- Game.java
+++ Game.java
@@ -3,11 +3,11 @@ import src.Field;
 class Game{
     
     public static void main(String [] args){
-        String[][] screen;
         Field field = new Field(30, 150);
 
-        screen = field.createField();
-        field.printField(screen);
+        field.createField();
+        field.randomField();
+        field.printField();
 
     }
 
--- src/Field.java
+++ src/Field.java
@@ -1,43 +1,56 @@
 package src;
 
+import java.util.Random;
+
 public class Field{
     
     // Declaring our variables
     int screenY;
     int screenX;
-    String[][] screen;
+    boolean[][] screen;
 
+    //Constructor
     public Field(int screenY, int screenX){
         this.screenY = screenY;
         this.screenX = screenX;
     }
 
-    public String[][] createField(){
-        /** Return the array field defined with the values defined in the object */
-
-        screen = new String[screenY][screenX];
+    public void createField(){
+        /** Assign the value to screen variable */
+        screen = new boolean[screenY][screenX];
 
         for (int y = 0; y < screenY; y++){
             for (int x = 0; x < screenX; x++){
-                screen[y][x] = " ";
+                screen[y][x] = false;
             }
         }
-        return screen;
     }
 
-    public void printField(String[][] screen){
-        //little test to see if the screen works
+    public void printField(){
+        //Pick the boolean array and transform to visual
         String visual = "";
 
-        for (String[] row : screen){
-            for (String field : row){
-                visual += "█";
+        for (boolean[] row : screen){
+            for (boolean field : row){
+                visual += field ? "█" : " ";
             }
 
             visual += "\n";
         }
 
         System.out.println(visual);
+    }
+
+    public void randomField(){
+        // Generate a random pattern of alive and dead cells
+        Random r = new Random();
+        boolean alive;
+
+        for (boolean[] row : screen){
+            for (int i = 0; i < row.length; i++){
+                row[i] = r.nextBoolean();
+            }
+        }
     }
 
 }
